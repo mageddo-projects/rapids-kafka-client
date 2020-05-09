b141832f7aa6eb1a54964d67687c90da51a09813
Elvis de Freitas <edigitalb@gmail.com>
2020-05-09 00:13:42 -0300

    creating utility methd

--- src/main/java/com/mageddo/kafka/client/DefaultCallbackContext.java
+++ src/main/java/com/mageddo/kafka/client/DefaultCallbackContext.java
@@ -1,8 +1,12 @@
 package com.mageddo.kafka.client;
 
+import java.util.Collections;
+
 import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.MockConsumer;
+import org.apache.kafka.clients.consumer.OffsetResetStrategy;
 
 import lombok.Builder;
 import lombok.NonNull;
@@ -10,7 +14,7 @@ import lombok.Value;
 import lombok.experimental.Accessors;
 
 @Value
-@Builder
+@Builder(toBuilder = true)
 @Accessors(fluent = true)
 public class DefaultCallbackContext<K, V> implements CallbackContext<K, V> {
   @NonNull
@@ -20,4 +24,13 @@ public class DefaultCallbackContext<K, V> implements CallbackContext<K, V> {
   private ConsumerRecords<K, V> records;
 
   private ConsumerRecord<K, V> record;
+
+  public static<K, V> DefaultCallbackContext<K, V> NOP(){
+    return DefaultCallbackContext
+        .<K, V>builder()
+        .records(new ConsumerRecords<>(Collections.EMPTY_MAP))
+        .consumer(new MockConsumer<>(OffsetResetStrategy.NONE))
+        .build()
+    ;
+  }
 }
